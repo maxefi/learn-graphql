4c977325e8edbabfe456a428a2fdf58c7a4eafed
maxefi <maxefi@mail.ru>
2020-01-13 16:01:16 +0300

    try null

--- src/index.js
+++ src/index.js
@@ -8,9 +8,13 @@ const typeDefs = `
 `
 
 // implementation of the GraphQL schema
+// one of the core benefits of GraphQL in general:
+// It enforces that the API actually behaves in the way that is promised by the schema definition!
+// This way, everyone who has access to the GraphQL schema can always be 100% sure about the API operations
+// and data structures that are returned by the API.
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
+    info: () => null
   }
 };
 
