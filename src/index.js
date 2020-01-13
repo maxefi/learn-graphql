60d6efbed08a0da4be6aace0d0c0a6ad02d7c730
maxefi <maxefi@mail.ru>
2020-01-13 19:45:23 +0300

    extended the schema definition with feed query and Link type

--- src/index.js
+++ src/index.js
@@ -4,8 +4,21 @@ const { GraphQLServer } = require('graphql-yoga');
 const typeDefs = `
   type Query {
     info: String!
+    feed: [Link!]!
+  }
+  
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `
+
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL',
+}];
 
 // implementation of the GraphQL schema
 // one of the core benefits of GraphQL in general:
@@ -14,8 +27,14 @@ const typeDefs = `
 // and data structures that are returned by the API.
 const resolvers = {
   Query: {
-    info: () => null
-  }
+    info: () => null,
+    feed: () => links,
+  },
+  Link: {
+    id: (parent) => parent.id,
+    url: (parent) => parent.url,
+    description: (parent) => parent.description,
+  },
 };
 
 const server = new GraphQLServer({
