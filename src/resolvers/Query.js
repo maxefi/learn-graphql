7d4267585c44a5dc290a953581c8c6045aeb450e
maxefi <m.efimov@itransition.com>
2020-04-06 13:42:06 +0300

    add feed filtering

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,4 +1,18 @@
-const feed = (root, args, context, info) => context.prisma.links();
+const feed = async (root, args, context, info) => {
+  const where = args.filter ? {
+    OR: [
+      { description_contains: args.filter },
+      { url_contains: args.filter },
+    ],
+  } : {}
+
+  const links = await context.prisma.links({
+    where
+  })
+
+  return links
+}
+
 const link = (root, args, context, info) => {
   console.log({ id: args.id });
   return context.prisma.links({ where: { id: args.id } });
